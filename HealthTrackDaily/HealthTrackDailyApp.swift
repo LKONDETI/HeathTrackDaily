8aef48b74807076de63352eccb73466e97122f63
Sravani K <sravanikondeti4@gmail.com>
2025-11-28 18:53:08 -0500

    Initial Commit

---
+++ HealthTrackDaily/HealthTrackDailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthTrackDailyApp.swift
+//  HealthTrackDaily
+//
+//  Created by Lakshmi Kondeti on 11/28/25.
+//
+
+import SwiftUI
+
+@main
+struct HealthTrackDailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HealthTrackDailyTests/HealthTrackDailyTests.swift
@@ -0,0 +1,17 @@
+//
+//  HealthTrackDailyTests.swift
+//  HealthTrackDailyTests
+//
+//  Created by Lakshmi Kondeti on 11/28/25.
+//
+
+import Testing
+@testable import HealthTrackDaily
+
+struct HealthTrackDailyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
